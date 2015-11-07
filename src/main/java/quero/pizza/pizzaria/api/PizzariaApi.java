36131cf4bac50326dc0ceb05c4d31416ccb5e522
marlonsouza <marlon.souuza@gmail.com>
2015-11-07 16:41:23 -0200

    Add DELETE

--- src/main/java/quero/pizza/pizzaria/api/PizzariaApi.java
+++ src/main/java/quero/pizza/pizzaria/api/PizzariaApi.java
@@ -5,14 +5,18 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import quero.pizza.common.GenericDAO;
+import quero.pizza.pizzaria.Cardapio;
 import quero.pizza.pizzaria.Pizzaria;
 
 @Stateless
@@ -40,5 +44,14 @@ public class PizzariaApi {
 		
 		return Response.status(Status.CREATED).entity(saved).build();
 		
+	}
+	
+	@DELETE
+	@Path("{id}")
+	public Response delete(@PathParam("id") Long id){
+		
+		pizzariaDAO.excluir(id, Pizzaria.class);
+		
+		return Response.ok().build();
 	}
 }
